13814a2a23fe334219f8a6bf04a77d9d41f060cc
avijit90 <sepiaavi@gmail.com>
2019-07-11 15:07:42 +0800

    CORS added.

--- src/main/java/com/rest/controller/HelloController.java
+++ src/main/java/com/rest/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.rest.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
 @RequestMapping(path = "/hello")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class HelloController {
 
     @GetMapping(path = "", produces = APPLICATION_JSON_VALUE)
