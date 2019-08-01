57920a9962a13d952dc04d85375e6c395e0cad60
Avijit Vaidya <sepiaavi@gmail.com>
2019-08-01 20:03:01 +0800

    Replaced raw string literals with constants and using MessageFormat for logging.

--- backend/main/java/com/rest/filter/RequesterLoggingFilter.java
+++ backend/main/java/com/rest/filter/RequesterLoggingFilter.java
@@ -4,11 +4,17 @@ import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+
+import static java.text.MessageFormat.format;
 
 @Component
 @Order(1)
 public class RequesterLoggingFilter implements Filter {
+
+    public static final String DESTROYING_REQUEST_LOGGING_FILTER = "Destroying request logging filter.";
+    public static final String REQUEST_LOGGING_FILTER_STARTED = "Request Logging filter started successfully";
 
     @Override
     public void doFilter(
@@ -17,7 +23,9 @@ public class RequesterLoggingFilter implements Filter {
             FilterChain chain) throws IOException, ServletException {
 
         System.out.println("---------------------------------");
-        System.out.println("Requester details :" + request.getRemoteAddr());
+
+        System.out.println(format("Incoming request, requester_details={0} and request_url={1}",
+                request.getRemoteAddr(), ((HttpServletRequest) request).getRequestURI()));
 
         chain.doFilter(request, response);
 
@@ -25,12 +33,12 @@ public class RequesterLoggingFilter implements Filter {
 
     @Override
     public void destroy() {
-        System.out.println("Destroying request logging filter.");
+        System.out.println(DESTROYING_REQUEST_LOGGING_FILTER);
     }
 
     @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-        System.out.println("Request Logging filter is up !");
+    public void init(FilterConfig filterConfig) {
+        System.out.println(REQUEST_LOGGING_FILTER_STARTED);
     }
 
 }
