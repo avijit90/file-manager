f391035977d35b180744caf577f674229173afef
Avijit Vaidya <sepiaavi@gmail.com>
2019-08-02 20:40:17 +0800

    Replaced raw strings with constants and renamed method to a more logical name.

--- backend/main/java/com/rest/controller/HelloController.java
+++ backend/main/java/com/rest/controller/HelloController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
@@ -14,13 +15,17 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class HelloController {
 
+    public static final String CURRENT_TIME_AT_SERVER_STRING = "Current time at server : ";
+    public static final String DATE_TIME_FORMAT = "dd/MM/yyyy HH:mm:ss.SSS";
+
     @GetMapping(path = "", produces = APPLICATION_JSON_VALUE)
     public String getHello() {
         return "{\"response\": {\"text\": \"Hello World !\"}}";
     }
 
     @GetMapping("/currentTime")
-    public String hello() {
-        return "Current time at server : " + new Date() + "\n";
+    public String getCurrentTime() {
+        String formattedDate = new SimpleDateFormat(DATE_TIME_FORMAT).format(new Date());
+        return CURRENT_TIME_AT_SERVER_STRING + formattedDate;
     }
 }
